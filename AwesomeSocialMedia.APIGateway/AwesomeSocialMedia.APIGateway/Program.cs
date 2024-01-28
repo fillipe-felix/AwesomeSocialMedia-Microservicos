f413c5be353e2e2905eb67a1d916ccff812fbde0
fillipe.felix <felipesoares_1993@hotmail.com>
2024-01-28 12:46:06 -0300

    adicionando configuração para abrir o swagger no api gateway

--- AwesomeSocialMedia.APIGateway/AwesomeSocialMedia.APIGateway/Program.cs
+++ AwesomeSocialMedia.APIGateway/AwesomeSocialMedia.APIGateway/Program.cs
@@ -1,30 +1,50 @@
+using AwesomeSocialMedia.APIGateway.Config;
+
+using MMLib.SwaggerForOcelot.DependencyInjection;
+
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+var routes = "Routes";
 
-// Add services to the container.
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Swagger for ocelot
 builder.Services.AddSwaggerGen();
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("ocelot.json")
-    .Build();
-
-builder.Services.AddOcelot(configuration);
-
 var app = builder.Build();
-
-await app.UseOcelot();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
+
+app.MapControllers();
 
 app.Run();
